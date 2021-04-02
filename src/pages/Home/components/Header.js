be38ed8bd45f418dc5b303f582141bc5200cdf37
Gustavo Silva <gustavocsil60@gmail.com>
2021-04-02 16:41:33 -0300

    Inition Code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Header ( ) {
+
+    return (
+    <header className="header"> 
+    <div className="toolbar">
+        <div className="">
+           <span>Conecta Dev</span>
+        </div>
+
+        <div className="">
+            <button>Novo Post</button>
+            <span>Imagem1</span>
+            <span>Imagem2</span>
+
+        </div>
+    </div>
+</header>
+)
+
+}
+
+export default Header;
